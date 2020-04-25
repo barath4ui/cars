3201095298346dded9455c16b929c135ab5ad348
Barathkumar <34671963+barath4ui@users.noreply.github.com>
2020-04-25 21:32:29 +0530

    cars 10

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
     const underline = document.querySelector(".underline");
 
     const cars = document.getElementsByClassName("cars");
+    const more = document.getElementsByClassName("car-btn");
 
     underline.style.width = items[0].offsetWidth +'px';
 
@@ -15,6 +16,18 @@
     for(let i=0; i<items.length; i++){
         const li = items[i];
         const car = cars[i];
+        const detail = more[i];
+
+        detail.addEventListener("click", function(){
+            
+            document.querySelector(".btn-search").classList.add("d-none");
+            document.querySelector(".heading").classList.add("d-none");
+            document.querySelector(".cars-details").classList.add("d-block");
+            document.querySelector(".btn-back").classList.add("d-block");
+
+
+        });
+
         li.addEventListener("click", function(){
 
             document.querySelector(".active").classList.remove("active");
@@ -32,7 +45,8 @@
             underline.style.left = sizeToMove + 'px';
             underline.style.width = li.offsetWidth + 'px';
 
-        });
+        }
+        );
     }
 
     
